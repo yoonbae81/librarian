99dc95ac60e1eb5a99baaa6b8c9b52a115d4ea90
Yoonbae Cho <y@xcv.kr>
2024-05-06 21:05:51 +0900

    Create a Visual Studio 2022 Solution

---
+++ src/Librarian/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Librarian;
+
+public class Class1
+{
+
+}
---
+++ tests/Test.Unit.Domain/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Test.Unit.Domain;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
