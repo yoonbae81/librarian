5a53702ebcd7cdddc576efd3f5f069c50596f421
Yoonbae Cho <y@xcv.kr>
2024-05-06 21:19:40 +0900

    Update

---
+++ src/Librarian/Entities/Book.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Librarian.Entities;
+public class Book(string title, string author)
+{
+    public string? Title { get; }
+    public string? Author { get; }
+}
---
+++ src/Librarian/Ports.Output/LibraryPort.cs
@@ -0,0 +1,12 @@
+﻿using Librarian.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librarian.Ports.Output;
+internal interface LibraryPort
+{
+    public string SearchCallNumber(Book book);
+}
